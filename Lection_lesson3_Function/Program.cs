9965c6988072b3dc0911880a3ef55b3378b0502d
Gosha117 <kometa210@gmail.com>
2024-04-18 14:47:50 +0300

    ädd lesson3_Function Task1

---
+++ Lection_lesson3_Function/Program.cs
@@ -0,0 +1,16 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    double denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+double result = CalculateFormula(1,2,3,4);
+Console.WriteLine(result);
+
+
+
+
+
+
+
